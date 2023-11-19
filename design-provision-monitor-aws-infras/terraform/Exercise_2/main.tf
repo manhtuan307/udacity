1a1f699c833fe5675309ffa059869043789f8fb7
tuan307 <manhtuan307@gmail.com>
2023-11-19 23:23:05 +0700

    Add permission to log cloud watch

--- design-provision-monitor-aws-infras/terraform/Exercise_2/main.tf
+++ design-provision-monitor-aws-infras/terraform/Exercise_2/main.tf
@@ -12,6 +12,12 @@ provider "aws" {
   region  = var.aws_region
 }
 
+data "archive_file" "python_lambda_package" {  
+  type = "zip"  
+  source_file = "greet_lambda.py" 
+  output_path = "greet_lambda.zip"
+}
+
 data "aws_iam_policy_document" "lambda_assum_role_policy"{
   statement {
     effect  = "Allow"
@@ -23,15 +29,33 @@ data "aws_iam_policy_document" "lambda_assum_role_policy"{
   }
 }
 
+resource "aws_iam_policy" "lambda_function_logs_policy" {
+  name        = "lambda_function_logs_policy"
+  path        = "/"
+  policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+      {
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect: "Allow"
+        Resource: "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role" "lambda_role" {  
-  name = "lambda-exec-role"  
+  name = "lambda-execution-role"  
   assume_role_policy = data.aws_iam_policy_document.lambda_assum_role_policy.json
 }
 
-data "archive_file" "python_lambda_package" {  
-  type = "zip"  
-  source_file = "greet_lambda.py" 
-  output_path = "greet_lambda.zip"
+resource "aws_iam_role_policy_attachment" "lambda_function_logs_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_function_logs_policy.arn
 }
 
 resource "aws_lambda_function" "demo_lambda_function" {
@@ -48,6 +72,8 @@ resource "aws_lambda_function" "demo_lambda_function" {
     environment{
       variables = {
         greeting = "Demo Python Lambda Function"
+      }
     }
-  }
+
+    depends_on = [aws_iam_role_policy_attachment.lambda_function_logs_policy]
 }
