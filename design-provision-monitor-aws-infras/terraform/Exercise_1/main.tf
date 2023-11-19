b7ea2dbef78de23882e46343b0eee0174039b92f
tuan307 <manhtuan307@gmail.com>
2023-11-19 23:37:41 +0700

    Update task and script

--- design-provision-monitor-aws-infras/terraform/Exercise_1/main.tf
+++ design-provision-monitor-aws-infras/terraform/Exercise_1/main.tf
@@ -10,20 +10,20 @@ terraform {
 
 provider "aws" {
   profile = "udacity"
-  region  = "us-east-1"
+  region  = "us-east-2"
 }
 
 
 # provision 4 AWS t2.micro EC2 instances named Udacity T2
 
 locals {
-  public_subnet_id = "subnet-0f2cdcde74c3fc239"
+  public_subnet_id = "subnet-08c37d68accfcb80a"
   resource_tag = "udacity-terraform"
 }
 
 resource "aws_instance" "t2_instances" {
   count         = 4
-  ami           = "ami-0230bd60aa48260c6"
+  ami           = "ami-06d4b7182ac3480fa"
   instance_type = "t2.micro"
   subnet_id     = local.public_subnet_id
   tags = {
@@ -37,7 +37,7 @@ resource "aws_instance" "t2_instances" {
 
 resource "aws_instance" "m4_instances" {
   count         = 2
-  ami           = "ami-0230bd60aa48260c6"
+  ami           = "ami-06d4b7182ac3480fa"
   instance_type = "m4.large"
   subnet_id     = local.public_subnet_id
   tags = {
