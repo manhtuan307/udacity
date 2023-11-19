4f6719157b7ee94443e56bfa8e4f8ad2e845a67b
tuan307 <manhtuan307@gmail.com>
2023-11-19 16:49:22 +0700

    Update terraform Ex1

--- design-provision-monitor-aws-infras/terraform/Exercise_1/main.tf
+++ design-provision-monitor-aws-infras/terraform/Exercise_1/main.tf
@@ -17,7 +17,6 @@ provider "aws" {
 # provision 4 AWS t2.micro EC2 instances named Udacity T2
 
 locals {
-  vpc_id = "vpc-093b0d88027d78020",
   public_subnet_id = "subnet-0f2cdcde74c3fc239"
   resource_tag = "udacity-terraform"
 }
@@ -26,7 +25,6 @@ resource "aws_instance" "t2_instances" {
   count         = 4
   ami           = "ami-0230bd60aa48260c6"
   instance_type = "t2.micro"
-  vpc_id        = local.vpc_id
   subnet_id     = local.public_subnet_id
   tags = {
     "Group"   = local.resource_tag
@@ -41,7 +39,6 @@ resource "aws_instance" "m4_instances" {
   count         = 2
   ami           = "ami-0230bd60aa48260c6"
   instance_type = "m4.large"
-  vpc_id        = local.vpc_id
   subnet_id     = local.public_subnet_id
   tags = {
     "Group"   = local.resource_tag
